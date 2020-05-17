06a22f558a2f6efd337afc194470dde3a21f4220
Ben Turner <bbgrabbag@gmail.com>
2020-05-16 23:51:35 -0600

    updated tasks to run dev scripts concurrently

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { server } from './server'
 import './config'
 
-server.listen(process.env.PORT, () => console.log('Server running on port ' + process.env.PORT))
+server.listen(process.env.PORT, () => console.log('Server running on port: ' + process.env.PORT))
